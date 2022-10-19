5d49ae50023fc778cbc6bc13f3c10f015ec686d7
EseAlli <essealli.ea@gmail.com>
2022-10-19 07:50:00 +0100

    update count after marking all as read

--- notifications-page/index.js
+++ notifications-page/index.js
@@ -1,8 +1,10 @@
 const markAsRead = document.querySelector(".mark");
 const unread = Array.from(document.querySelectorAll(".unread"));
+const count = document.querySelector(".count");
 
 markAsRead.addEventListener("click", () => {
   unread.forEach((un) => {
     un.classList.remove("unread");
   });
+  count.innerHTML = 0;
 });
